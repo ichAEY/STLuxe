beb9947cf32963345607bcf5b400f05a9730ae26
tanem <melikyan.mac@gmail.com>
2026-09-11 01:28:49 +0300

    Tighten desktop header and remove visual gap

--- desktop-stluxe-current.js
+++ desktop-stluxe-current.js
@@ -38,7 +38,7 @@
       #stluxe-desktop-v1 button{cursor:pointer}
 
       .std-header{
-        height:98px;
+        height:76px;
         width:100%;
         display:flex;
         align-items:center;
@@ -72,7 +72,7 @@
       .std-phone{
         margin-left:auto;
         width:auto;
-        height:54px;
+        height:46px;
         display:flex;
         align-items:center;
         justify-content:center;
@@ -91,7 +91,7 @@
       .std-hero{
         display:grid;
         grid-template-columns:50% 50%;
-        height:calc(100svh - 98px);
+        height:calc(100svh - 76px);
         min-height:690px;
         margin:0;
         background:#242127;
